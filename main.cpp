4eb80575adf8d5ece9f79fce0ae5fa29ff45a985
Egor2034 <egor_2061@mail.ru>
2025-10-12 16:36:33 +0400

    Реализованы коснтруктор по умолчанию, конструктор с параметром capacity, конструктор с массивом элементов, конструкторы генерации случайных чисел для целых и вещественных типов. Реализованы функция contains, функция swap, оператор вывода элементов множества

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#include "set.h"
 
 int main() {
     /*main*/
-   
+    Set<int> se(10, -10, 100);
     return 0;
 }
--- set.h
+++ set.h
@@ -2,19 +2,102 @@
 #define SET_H
 
 #include <iostream>
+#include <algorithm>
+#include <stdexcept>
+#include <random>
 /*
 ДОБАВИТЬ ФУНКЦИЮ SWAP
 */
 template<typename T> requires std::equality_comparable<T>
 class Set {
 public:
-    Set();
-    Set(size_t size);
-    Set(const T* el);
-    Set(int count, int min, int max) requires std::integral<T> || std::floating_point<T>;
+    Set() {
+        _data(nullptr); 
+        _count = 0; 
+        _capacity = 0;
+    }
 
-    Set(const Set& other);
-    Set(const Set&& other);
+    Set(size_t capacity) {
+        _count = 0;
+        _capacity = capacity;
+        _data = new T[_capacity];
+    }
+
+    Set(const T* arr, size_t size) {
+        _capacity = size;
+        _data = new T[size];
+
+        for (size_t i = 0; i < size; i++) {
+            if (!contains(arr[i])) { 
+                _count++;
+                _data[i] = arr[i]; 
+            }
+        }
+    }
+
+    Set(size_t count, T min, T max) requires std::integral<T> {
+        if (min > max) std::swap(min, max);
+
+        if (max - min + 1 < count) {
+            throw std::invalid_argument("The number of unique elements is less than the required set size");
+        }
+
+        if (count == 0) {
+            _count = 0;
+            _capacity = 0;
+            _data = nullptr;
+            return;
+        }
+
+        std::random_device seed;
+        std::mt19937 generator(seed());
+        std::uniform_int_distribution distribution(min, max);
+
+        _count = count;
+        _capacity = count;
+        _data = new T[count];
+        
+        bool* used = new bool[max - min + 1]();
+
+        for (size_t i = 0; i < count; i++) {
+            T number;
+
+            do {
+                number = distribution(generator);
+            } while (used[number - min]);
+
+            used[number - min] = true;
+            _data[i] = number;
+        }
+
+        delete[] used;
+    }
+
+    Set(size_t count, T min, T max) requires std::floating_point<T> {
+        if (min > max) std::swap(min, max);
+
+        if (count == 0) {
+            _count = 0;
+            _capacity = 0;
+            _data = nullptr;
+            return;
+        }
+       
+        std::random_device seed;
+        std::mt19937 generator(seed());
+        std::uniform_real_distribution distribution(min, max);
+
+        _count = count;
+        _capacity = count;
+        _data = new T[count];
+        
+        for (size_t i = 0; i < count; i++) {
+            _data[i] = distribution(generator);
+        }
+    }
+
+    Set(const Set<T>& other);
+    Set(const Set<T>&& other);
     ~Set() { delete[] _data; }
 
     Set<T>& operator=(const Set<T>& other);
@@ -26,7 +109,7 @@ public:
     Set<T>& operator+=(const Set<T>& other);
     Set<T> operator-(const Set<T>& other) const;
     Set<T>& operator-=(const Set<T>& other);
-    
+
     /*Операторы для работы с элементом типа T*/
     Set<T> operator+(const T& el) const;
     Set<T>& operator+=(const T& el);
@@ -36,28 +119,48 @@ public:
     bool operator==(const Set<T>& other) const;
     bool operator!=(const Set<T>& other) const;
 
-    bool contains(const T& el) const;
+    bool contains(const T& el) const {
+        for (size_t i = 0; i < _count; i++) {
+            if (_data[i] == el) return true;
+        }
+
+        return false;
+    }
+
     void add(const T& el);
     void clear();
-    //void sort();
     size_t get_size() const;
     size_t get_capacity() const;
-    
-    T* begin();
-    const T* begin() const;
-    T* end() const;
-    const T* end() const;
+
+    //T* begin();
+    //const T* begin() const;
+    //T* end() const;
+    //const T* end() const;
 private:
-    size_t _size;
+    size_t _count;
     size_t _capacity;
     T* _data;
 
-    const float EPSILON = 1e-6f;
-    friend std::ostream& operator<<(std::ostream& os, const Set<T>& set);
-    friend Set<T> intersection(const Set<T> first, const Set<T> second);
+    inline static const double EPSILON = 1e-6  ;
+
+    friend std::ostream& operator<<(std::ostream& os, const Set<T>& set) {
+        for (size_t i = 0; i < set._count; i++) {
+            os << set._data[i] << "\n";
+        }
+        return os;
+    }
+
+    friend Set<T> intersection(const Set<T>& first, const Set<T>& second);
 };
 
-template<typename T>
-void swap(Set<T> first, Set<T> second) {}
+template <typename T>
+void swap(Set<T>& first, Set<T>& second) {}
+
+template <typename T>
+void swap(T& first, T& second) {
+    T temp = first;
+    first = second;
+    second = temp;
+}
 
 #endif
